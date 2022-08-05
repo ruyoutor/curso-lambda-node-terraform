7f5d24d750c92ec421bb40733ff94b1ca9df577f
Ruy Outor <ruyoutor11@gmail.com>
2022-08-05 16:01:13 -0300

    Installed nodemon

--- src/service/BmiCalcService.js
+++ src/service/BmiCalcService.js
@@ -3,14 +3,14 @@ const logger = require('../logger/Logger').Logger;
 class BmiCalcService {
     constructor(){
         this.performBmiCalculation = (w, h) => {
-            logger.info(`BmiCalcService@perfomBmiCalculation: w=${w}, h=${h}`);
+            logger.info(`BmiCalcService#perfomBmiCalculation: w=${w}, h=${h}`);
             if (h == 0){
                 throw Error("Height must be > 0")
             }
 
             let weight = parseFloat(w);
             let height = parseFloat(h);
-            logger.info(`BmiCalcService@perfomBmiCalculation: weight=${weight}, height=${height}`);
+            logger.info(`BmiCalcService#perfomBmiCalculation: weight=${weight}, height=${height}`);
             return (weight / height / height * 10000).toFixed(2)
         }
     }
